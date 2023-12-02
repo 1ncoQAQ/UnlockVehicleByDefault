1b3769d41fa18001aab127be756567ca714ddf64
YincongY <112535170+YincongY@users.noreply.github.com>
2023-12-03 06:24:11 +0800

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnlockVehicle")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b9d1825d-0386-4d7a-847e-53ce0535aee1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
---
+++ UnlockVehicle.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+public class UnlockVehicle : IModApi // ModEntryPoint - RESERVED LOOKUP NAME
+{
+    public void InitMod(Mod _modInstance)
+    {
+        Harmony harmony = new Harmony(base.GetType().ToString());
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+
+    [HarmonyPatch(typeof(EntityVehicle), "Awake")]
+    public class EntityVehicle_Awake
+    {
+        [HarmonyPostfix]
+        public static void postfix(EntityVehicle __instance)
+        {
+            __instance.isLocked = false;
+        }
+    }
+}
